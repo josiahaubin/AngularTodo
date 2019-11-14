0d47d7e959f978f6c62a5c3f232b3125f39e583c
Josiah Aubin <josiahaubin@gmail.com>
2019-11-14 13:28:14 -0700

    Can add todos to array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angtodo';
   todo = '';
+  todoArray = [];
+
+  addTodo(value) {
+    this.todoArray.push(value);
+    console.log(this.todoArray)
+  }
 }
